57e21939e23236ee1e4c2d4bcd82ad7058694168
sho sato <oydgw36@gmail.com>
2018-11-29 23:19:24 +0900

    add flask-wtf

--- hello.py
+++ hello.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html',
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name,
             current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
