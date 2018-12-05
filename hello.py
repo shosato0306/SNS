a1b35008c81e705360f177fdfb530af73822c17a
sho sato <oydgw36@gmail.com>
2018-12-05 12:01:07 +0900

    add send mail function to index.py

--- hello.py
+++ hello.py
@@ -26,7 +26,8 @@ app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <satoflasktest@gmail.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
@@ -70,6 +71,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'NEW USER',
+                    'mail/new_user', user=user)
             flash('User registration to database is succeeded')
         else: 
             session['known'] = True
