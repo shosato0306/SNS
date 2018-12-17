087fcfc7d9fc073d36e280bda9590bbf88ae9b42
sho sato <oydgw36@gmail.com>
2018-12-17 12:42:34 +0900

    add user role management

--- flasky.py
+++ flasky.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission
 from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -8,8 +8,10 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
+    """flask The shell command invokes this function
+    with a shell_context_processor decorator every time it starts up""" 
     print("flasky.make_shell_context")
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
 
 @app.cli.command()
 def test():
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,6 +1,6 @@
 import unittest
 import time
-from app.models import User
+from app.models import User, AnonymousUser, Role, Permission
 from app import create_app, db
 
 class UserModelTestCase(unittest.TestCase):
@@ -9,6 +9,7 @@ class UserModelTestCase(unittest.TestCase):
         self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
+        Role.insert_roles()
 
     def tearDown(self):
         db.session.remove()
@@ -102,3 +103,36 @@ class UserModelTestCase(unittest.TestCase):
         self.assertFalse(u2.change_email(token))
         self.assertTrue(u2.email == 'susan@example.org')
     
+    def test_user_role(self):
+        u = User(email='john@example.com', password='cat')
+        self.assertTrue(u.can(Permission.FOLLOW))
+        self.assertTrue(u.can(Permission.COMMENT))
+        self.assertTrue(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
+    
+    def test_moderator_role(self):
+         r = Role.query.filter_by(name='Moderator').first()
+         u = User(email='john@example.com', password='cat', role=r)
+         self.assertTrue(u.can(Permission.FOLLOW))
+         self.assertTrue(u.can(Permission.COMMENT))
+         self.assertTrue(u.can(Permission.WRITE))
+         self.assertTrue(u.can(Permission.MODERATE))
+         self.assertFalse(u.can(Permission.ADMIN))
+
+    def test_administrator_role(self):
+         r = Role.query.filter_by(name='Administrator').first()
+         u = User(email='john@example.com', password='cat', role=r)
+         self.assertTrue(u.can(Permission.FOLLOW))
+         self.assertTrue(u.can(Permission.COMMENT))
+         self.assertTrue(u.can(Permission.WRITE))
+         self.assertTrue(u.can(Permission.MODERATE))
+         self.assertTrue(u.can(Permission.ADMIN))
+        
+    def test_anonymous_user(self):
+        u = AnonymousUser()
+        self.assertFalse(u.can(Permission.FOLLOW))
+        self.assertFalse(u.can(Permission.COMMENT))
+        self.assertFalse(u.can(Permission.WRITE))
+        self.assertFalse(u.can(Permission.MODERATE))
+        self.assertFalse(u.can(Permission.ADMIN))
