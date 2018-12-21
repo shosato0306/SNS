a086b752eb9f8df7132dcab7fe11ece7181f080e
sho sato <oydgw36@gmail.com>
2018-12-21 21:32:59 +0900

    add post article func and display article func

--- app/main/views.py
+++ app/main/views.py
@@ -3,37 +3,51 @@ from flask import render_template, session, redirect, url_for, \
                 current_app, flash
 from flask_login import login_required, current_user
 from . import main
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 from ..import db
-from ..models import User, Role
+from ..models import Permission, User, Role, Post
 from ..decorators import admin_required
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'NEW USER',
-                    'mail/new_user', user=user)
-            flash('User registration to database is succeeded')
-        else: 
-            session['known'] = True
-            flash('Your name is already registerd')
-        session['name'] = form.name.data
-        form.name.data = ""
+    form = PostForm()
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
         return redirect(url_for('.index'))
-    return render_template('index.html',
-                            form=form, 
-                            name=session.get('name'),
-                            known=session.get('known', False),
-                            current_time=datetime.utcnow())
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
+        
+
+
+# @main.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.name.data).first()
+#         if user is None:
+#             user = User(username=form.name.data)
+#             db.session.add(user)
+#             db.session.commit()
+#             session['known'] = False
+#             if current_app.config['FLASKY_ADMIN']:
+#                 send_email(current_app.config['FLASKY_ADMIN'], 'NEW USER',
+#                     'mail/new_user', user=user)
+#             flash('User registration to database is succeeded')
+#         else: 
+#             session['known'] = True
+#             flash('Your name is already registerd')
+#         session['name'] = form.name.data
+#         form.name.data = ""
+#         return redirect(url_for('.index'))
+#     return render_template('index.html',
+#                             form=form, 
+#                             name=session.get('name'),
+#                             known=session.get('known', False),
+#                             current_time=datetime.utcnow())
 
 @main.route('/user/<username>')    
 def user(username):
@@ -83,3 +97,4 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
