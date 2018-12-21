e9f1e9ca0c5f394164112df233b01584dfcae90c
sho sato <oydgw36@gmail.com>
2018-12-21 21:47:09 +0900

    add display posts func to user profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, \
-                current_app, flash
+                current_app, flash, abort
 from flask_login import login_required, current_user
 from . import main
 from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
@@ -51,8 +51,11 @@ def index():
 
 @main.route('/user/<username>')    
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
