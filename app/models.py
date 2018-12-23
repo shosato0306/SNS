aea2a3b823869cdb5411212a7334e6d8f8e2e295
sho sato <oydgw36@gmail.com>
2018-12-23 15:42:36 +0900

    add Follow model

--- app/models.py
+++ app/models.py
@@ -68,6 +68,16 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>'% self.name
 
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                    primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('users.id'),
+                    primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+
 class User(UserMixin, db.Model):
     __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
@@ -83,6 +93,16 @@ class User(UserMixin, db.Model):
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     avatar_hash = db.Column(db.String(32))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship('Follow',
+                foreign_keys=[Follow.follower_id],
+                backref=db.backref('follower', lazy='joined'),
+                lazy='dynamic',
+                cascade='all, delete-orphan')
+    followers = db.relationship('Follow',
+                foreign_keys=[Follow.followed_id],
+                backref=db.backref('followed', lazy='joined'),
+                lazy='dynamic',
+                cascade='all, delete-orphan')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -190,6 +210,28 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def follow(self, user):
+        if not self.is_following(user):
+            f = Follow(followed=user)
+            self.followed.append(f)
+
+    def unfollow(self, user):
+        f = self.followed.filter_by(followed_id=user.id).first()
+        if f:
+            self.followed.remove(f)
+
+    def is_following(self, user):
+        if user.id is None:
+            return False
+        return self.followed.filter_by(
+            followed_id=user.id).first() is not None
+
+    def is_followed_by(self, user):
+        if user.id is None:
+            return False
+        return self.followers.filter_by(
+            follower_id=user.id).first() is not None
+
     def __repr__(self):
         return '<User %r>'% self.username
 
@@ -226,6 +268,5 @@ class Post(db.Model):
                 tags=allowed_tags,
                 strip=True)
                 )
-
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
--- flasky.py
+++ flasky.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Follow, Role, Permission, Post
 from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -11,7 +11,8 @@ def make_shell_context():
     """flask The shell command invokes this function
     with a shell_context_processor decorator every time it starts up""" 
     print("flasky.make_shell_context")
-    return dict(db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+    return dict(db=db, User=User, Follow=Follow,
+                Role=Role, Permission=Permission, Post=Post)
 
 @app.cli.command()
 def test():
