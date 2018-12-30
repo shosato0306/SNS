7d5d26a29c4bda95a828ecf7b5f8e014dc92c19e
sho sato <oydgw36@gmail.com>
2018-12-30 13:06:50 +0900

    #15 implement deploy function

--- sns.py
+++ sns.py
@@ -10,7 +10,7 @@ import sys
 import click
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -46,18 +46,6 @@ def test(coverage):
 
 # @app.cli.command()
 # @click.option('--length', default=25,
-#                 help='Number of funtions to include in the profiler report.')
-# @click.option('--profile-dir', default=None,
-#                 help='Directory where profiler data files are saved.')
-# def profile(length, profile_dir):
-#     """Start the application under the code profiler."""
-#     from werkzeug.contrib.profiler import ProfilerMiddleware
-#     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
-#                                 profile_dir=profile_dir)
-#     app.run(debug=False)
-
-# @app.cli.command()
-# @click.option('--length', default=25,
 #                 help='Number of functions to include in the profiler report.')
 # @click.option('--profile-dir', default=None,
 #                 help='Directory where profiler data files are saved.')
@@ -66,4 +54,17 @@ def test(coverage):
 #     from werkzeug.contrib.profiler import ProfilerMiddleware
 #     app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
 #                                         profile_dir=profile_dir)
-#     app.run(debug=False)
+#     app.run()
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
+
+    # ensure all users are following themselves
+    User.add_self_follows()
+    
