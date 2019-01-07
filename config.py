8ec1a75f12360c565b6f3f33c15816ad6cde9b76
sho sato <oydgw36@gmail.com>
2019-01-08 00:10:03 +0900

    mod config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret key'
-    MAIL_SERVER = os.environ.get('MAIL_SEVER', 'smtp.gmail.com')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS','true').lower() in \
